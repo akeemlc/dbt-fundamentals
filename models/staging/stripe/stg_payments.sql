ffa9c597dbbdd35754278d5fbf8a2b9f61cb8e82
akeem.a@localcoinatm.com <akeem.a@localcoinatm.com>
2022-01-25 23:26:47 +0000

    Added documentation ref

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,16 +1,9 @@
-with payments as (
-
-    select
+select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-
-from raw.stripe.payment 
-)
-
-select * from payments
+from {{ source('stripe', 'payment') }}
