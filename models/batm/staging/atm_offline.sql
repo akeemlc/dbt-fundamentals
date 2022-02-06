6d7f8d3cad4500357372b93cf0063b95208263b6
Akeem Amure <akeem.a@localcoinatm.com>
2022-02-06 18:30:35 +0000

    UPDATES

--- models/batm/staging/atm_offline.sql
+++ models/batm/staging/atm_offline.sql
@@ -3,7 +3,7 @@ with downtime as (
  SELECT *
     FROM
          {{ source('batm', 'OFFLINE_ELAPSED_TIME') }}
-      --  "FIVETRAN_DATABASE"."MYSQL_RDS_BATM_CUSTOM"."OFFLINE_ELAPSED_TIME"
+       -- "FIVETRAN_DATABASE"."MYSQL_RDS_BATM_CUSTOM"."OFFLINE_ELAPSED_TIME"
 )
 
 select
