f8061d653a07c82b6bc34b2f46f8b7bc74d06042
Akeem Amure <akeem.a@localcoinatm.com>
2022-02-08 22:21:19 +0000

    added locationID

--- models/batm/marts/atm_daily.sql
+++ models/batm/marts/atm_daily.sql
@@ -2,7 +2,7 @@ with atm_performance as (
 
     select
          serialnumber,
-         cashamount , datetime ,month ,YEAR ,transaction_type ,city 
+         cashamount , datetime ,month ,YEAR ,transaction_type ,city ,deployedto,regioncode,location_id,province
     from fivetran_Database.mysql_rds_batm_dump.DMP_FINANCE_VOLUME_AND_TRANSACTIONS
 
 )
--- models/batm/marts/monthly_performance.sql
+++ models/batm/marts/monthly_performance.sql
@@ -1,18 +1,20 @@
 with a as 
 (
-  select LAST_DAY(to_date(DATETIME)) AS Month_end,serialnumber,  sum(cashamount) as volume, count(*) as Transaction_Count 
+  select LAST_DAY(to_date(DATETIME)) AS Month_end,serialnumber,  sum(cashamount) as volume, count(*) as Transaction_Count ,regioncode,province
   
   --from "ANALYTICS"."DBT_AKEEMLC"."PERFORMANCE" 
   from {{ ref('atm_daily')}}
 
  
-  where year = '2021' GROUP BY SERIALNUMBER,LAST_DAY(to_date(DATETIME))
+  where year = '2021' GROUP BY SERIALNUMBER,LAST_DAY(to_date(DATETIME)),regioncode,province
  ), final as 
  (
  select serialnumber
        ,Month_end
        ,volume 
+       ,Transaction_Count
        ,lag(volume) over (partition by serialnumber order by Month_end) previous_volume
+       ,regioncode, province
 from a
  )
  select 
@@ -21,6 +23,8 @@ from a
        ,Month_end
        ,volume 
        ,previous_volume
+       ,Transaction_Count
        ,concat(round((volume - previous_volume)  * 100 /previous_volume,0)) as "MoM%"
+       ,regioncode, province
 
 from final order by 2 desc
